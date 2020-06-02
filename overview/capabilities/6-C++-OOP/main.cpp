f13f822032843910147c5b2f883dc278896f33c8
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2020-06-02 15:13:26 -0500

    updates

--- overview/capabilities/6-C++-OOP/main.cpp
+++ overview/capabilities/6-C++-OOP/main.cpp
@@ -6,28 +6,39 @@
 using namespace std;
 
 template <typename T>
-static stack<T> *
+static unique_ptr<stack<T>>
 make_stack (bool use_vstack, size_t size) {
   if (use_vstack)
-    return new v_stack<T>(size);
+    return make_unique<v_stack<T>>(size);
   else
-    return new l_stack<T>(size);
+    return make_unique<l_stack<T>>(size);
 }
 
 template <typename T>
 static void print_top (stack<T> *sp) {
-  std::cout << "top = " 
-            << sp->top () // (*sp->vptr[3])(sp);
+  std::cout << "top item = "
+            << sp->top () // (*sp->vptr[n])(sp);
             << std::endl;
 }
 
 static const size_t MAX_SIZE = 10;
 
-int main(int argc, char *argv[]) {
+/**
+ * This example shows how using our abstract base class make it
+ * possible to write code that does not depend on the stack
+ * implementation.
+ */
+int
+main(int argc, char *argv[]) {
   std::unique_ptr <stack<int>> sp (make_stack<int> (argc > 1,
                                                       MAX_SIZE));
+  auto item = 0;
 
-  int item = 0;
+  auto *vsp = dynamic_cast<v_stack<int> *>(sp.get());
+  if (vsp)
+      cout << "configured with v_stack" << endl;
+  else
+      cout << "configured with l_stack" << endl;
 
   try {
     for (;;)
@@ -38,7 +49,7 @@ int main(int argc, char *argv[]) {
 
   try {
     for (;;) {
-      std::cout << "top item = " << sp->top() << std::endl;
+      print_top(sp.get());
       sp->pop();
     }
   } catch (stack<int>::underflow &ex) {
