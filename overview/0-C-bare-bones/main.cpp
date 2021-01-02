ea8fe3d927c01c36f548969924d53005be9c1c84
Jinshuo Zhang <jinshuoz@umd.edu>
2021-01-01 19:58:17 -0800

    fix crashing

--- overview/0-C-bare-bones/main.cpp
+++ overview/0-C-bare-bones/main.cpp
@@ -15,14 +15,16 @@ int main() {
   int top = 0;
 
   // Keep looping until the stack is "full".
-  while (top < MAX_STACK)
-    // Push items onto the stack;
-    stack[top] = top++;
+  while (top < MAX_STACK) {
+      // Push items onto the stack;
+      stack[top] = top;
+      top++;
+  }
 
   // Keep looping until the stack is "empty".
   while (--top >= 0)
     // Pop items off the stack and print them.
-    printf("top item = %d\n", stack[top]); 
+    printf("top item = %d\n", stack[top]);
 
   return 0;
 }
